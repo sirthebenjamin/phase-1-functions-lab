1c9535f40c41a14a0eb6f7363ddac9894ae38b99
sirthebenjamin <126521851+sirthebenjamin@users.noreply.github.com>
2023-03-30 18:33:25 -0400

    Adss myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(streetNumber) {
+    const hqStreetNumber = 42;
+    return Math.abs(streetNumber - hqStreetNumber);
+  }
+  
+  module.exports = {
+    distanceFromHqInBlocks
+  };
+
+  
+  function distanceFromHqInBlocks(streetNumber) {
+    const hqStreetNumber = 42;
+    return Math.abs(streetNumber - hqStreetNumber);
+  }
+  
+  function distanceFromHqInFeet(streetNumber) {
+    const distanceInBlocks = distanceFromHqInBlocks(streetNumber);
+    return distanceInBlocks * 264;
+  }
+  
+  function distanceTravelledInFeet(start, end) {
+    const distanceInBlocks = Math.abs(start - end);
+    return distanceInBlocks * 264;
+  }
+  
+  function calculatesFarePrice(start, end) {
+    const distanceInFeet = distanceTravelledInFeet(start, end);
+  
+    if (distanceInFeet <= 400) {
+      return 0;
+    } else if (distanceInFeet <= 2000) {
+      return (distanceInFeet - 400) * 0.02;
+    } else if (distanceInFeet <= 2500) {
+      return 25;
+    } else {
+      return 'cannot travel that far';
+    }
+  }
+  
+ 
+
+
+
+
+
+
